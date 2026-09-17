38345afd1b16d8d8b5c1dbb76d73d99ebbc31895
yana <yanagi.toshio@gmail.com>
2026-09-17 16:41:15 +0900

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,2 @@
+// Google Apps Script の「ウェブアプリ」URL（末尾が /exec）を貼り付けてください。
+// 例: window.KINTAI_API_URL = 'https://script.google.com/macros/s/AKfycbwNKMluhdMYLphGcdTS2_X6jQAUFW0E9SX-VKLaX7lmVcFikVaE6Pkpb5GBpy6nVPdv/exec';
