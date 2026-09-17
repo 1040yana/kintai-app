9ce9d40449c4c7c0a90515f6e99c656654b8d9f3
yana <yanagi.toshio@gmail.com>
2026-09-17 21:39:04 +0900

    Delete app-config.js

--- app-config.js
+++
@@ -1,2 +0,0 @@
-// Google Apps Script の「ウェブアプリ」URL（末尾が /exec）を貼り付けてください。
-// 例: window.KINTAI_API_URL = 'https://script.google.com/macros/s/AKfycbyqJ8wWCdkGSCW5g2Tv6F7qNa_C_dIcPAJ-whxe6CGbbQn0s79isWNQVyyW2SExqSyY/exec';
