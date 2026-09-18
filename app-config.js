f3f3a156ea9e55da1731c56526755b3418a483bd
yana <yanagi.toshio@gmail.com>
2026-09-18 20:23:09 +0900

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,2 +1,2 @@
 </> JavaScript
-window.KINTAI_API_URL='https://script.google.com/macros/s/AKfycbw6PdFBK-ukXLVJJPB-xuEncjdxZQfYZnklMZARruLuxUPBNq03QxYVmDHhwmKHLlk/exec';
+window.KINTAI_API_URL = 'https://script.google.com/macros/s/AKfycbw6PdFBK-ukXLVJJPB-xuEncjdxZQfYZnklMZARruLuxUPBNq03QxYVmDHhwmKHLlk/exec';
