a6afa174e7a5e7d3fc09e7ae68630a497d200b96
yana <yanagi.toshio@gmail.com>
2026-09-19 02:43:34 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'rakuraku-kintai-v10';
+const CACHE = 'rakuraku-kintai-v11';
 const APP_FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
